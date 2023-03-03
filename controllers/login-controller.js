3b93a6208fc75be421d9915a245a8df3ec8f6c4d
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-03-02 17:49:45 -0800

    Added create account function

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -1,18 +1,53 @@
+import jwt from "jsonwebtoken";
+
+let adminToken;
+let realUsername;
+let realPassword;
+const testId = new Date().getDate();
+
+const createAccount = async (req, res) => {
+    res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
+    try {
+        if (!req.body.username || !req.body.password) {
+            throw new Error("Credentials not provided");
+        } else {
+            realUsername = req.body.username;
+            realPassword = req.body.password;
+        }
+        const token = jwt.sign(
+            { id: testId, username: realUsername },
+            process.env.JWT_SECRET,
+            { expiresIn: "2d" }
+        );
+        adminToken = token;
+        res.status(200).json({ status: "success", token: adminToken });
+    } catch (error) {
+        console.error(err);
+        res.status(500).json({
+            msg: "There has been an error, please try again later",
+        });
+    }
+};
+
 const loginAttempt = async (req, res) => {
     res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
     try {
-        const authInfo = req.body;
-        const realUsername = "test";
-        const realPassword = "admin";
-        console.log(req.body);
-        const paramUsername = authInfo.user;
-        const paramPassword = authInfo.pass;
-        if (realUsername === paramUsername && realPassword === paramPassword) {
-            res.status(200);
-            res.json({ status: "success" });
-        } else {
-            throw new Error("Authentication failed");
+        const authInfo = req.headers.authorization;
+        const paramUsername = req.body.username;
+        const paramPassword = req.body.password;
+        if (realUsername !== paramUsername || realPassword !== paramPassword) {
+            throw new Error(
+                "Authentication failed, credentials do not match a valid user"
+            );
         }
+        if (!authInfo || !authInfo.includes("Bearer ")) {
+            throw new Error("No token found or invalid token format");
+        }
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(decoded);
+        res.status(200);
+        res.json({ status: "success" });
     } catch (err) {
         console.error(err);
         res.status(500).json({
@@ -22,10 +57,13 @@ const loginAttempt = async (req, res) => {
 };
 
 const optionsPreflight = (req, res) => {
+    res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
     try {
-        res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
         res.header("Access-Control-Allow-Methods", "POST");
-        res.header("Access-Control-Allow-Headers", ["content-type"]);
+        res.header("Access-Control-Allow-Headers", [
+            "content-type",
+            "authorization",
+        ]);
         res.status(200);
         res.json({ msg: "Preflight Passed" });
     } catch (err) {
@@ -36,4 +74,4 @@ const optionsPreflight = (req, res) => {
     }
 };
 
-export { optionsPreflight, loginAttempt };
+export { optionsPreflight, loginAttempt, createAccount };

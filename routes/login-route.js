6c4e52ddec9112de25d47ab525a54de29831c13f
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-03-02 17:48:46 -0800

    Added create route

--- routes/login-route.js
+++ routes/login-route.js
@@ -2,12 +2,15 @@ import express from "express";
 
 import {
     optionsPreflight,
+    createAccount,
     loginAttempt,
 } from "../controllers/login-controller.js";
 
 const loginRouter = express.Router();
 
 loginRouter.options("/", optionsPreflight);
+loginRouter.options("/create", optionsPreflight);
 loginRouter.post("/", loginAttempt);
+loginRouter.post("/create", createAccount);
 
 export { loginRouter };

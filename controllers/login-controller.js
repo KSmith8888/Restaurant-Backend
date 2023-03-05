22d0b148bcfd76b36f348881cd685ee0562f2b13
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-03-04 17:58:57 -0800

    Added user account database functionality

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -1,26 +1,29 @@
 import jwt from "jsonwebtoken";
 
+import { User } from "../models/user-model.js";
+
 let adminToken;
-let realUsername;
-let realPassword;
-const testId = new Date().getDate();
 
 const createAccount = async (req, res) => {
-    res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
     try {
+        res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
+        res.header("Strict-Transport-Security", "max-age=6000");
         if (!req.body.username || !req.body.password) {
             throw new Error("Credentials not provided");
         } else {
-            realUsername = req.body.username;
-            realPassword = req.body.password;
+            const userInfo = {
+                username: req.body.username,
+                password: req.body.password,
+            };
+            const dbUser = await User.create(userInfo);
+            const token = jwt.sign(
+                { id: dbUser._id, username: dbUser.username },
+                process.env.JWT_SECRET,
+                { expiresIn: "2h" }
+            );
+            adminToken = token;
+            res.status(200).json({ status: "success", token: token });
         }
-        const token = jwt.sign(
-            { id: testId, username: realUsername },
-            process.env.JWT_SECRET,
-            { expiresIn: "2d" }
-        );
-        adminToken = token;
-        res.status(200).json({ status: "success", token: adminToken });
     } catch (error) {
         console.error(err);
         res.status(500).json({
@@ -30,24 +33,38 @@ const createAccount = async (req, res) => {
 };
 
 const loginAttempt = async (req, res) => {
-    res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
     try {
-        const authInfo = req.headers.authorization;
-        const paramUsername = req.body.username;
-        const paramPassword = req.body.password;
-        if (realUsername !== paramUsername || realPassword !== paramPassword) {
+        res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
+        res.header("Strict-Transport-Security", "max-age=6000");
+        if (
+            !req.headers.authorization ||
+            !req.body.username ||
+            !req.body.password
+        ) {
             throw new Error(
-                "Authentication failed, credentials do not match a valid user"
+                "Authentication failed, proper credentials are not present"
             );
         }
-        if (!authInfo || !authInfo.includes("Bearer ")) {
-            throw new Error("No token found or invalid token format");
+        if (!req.headers.authorization.includes("Bearer ")) {
+            throw new Error("Invalid token format");
         }
         const token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log(decoded);
-        res.status(200);
-        res.json({ status: "success" });
+        const decodedClient = jwt.verify(token, process.env.JWT_SECRET);
+        const decodedSaved = jwt.verify(adminToken, process.env.JWT_SECRET);
+        const id = decodedClient.id;
+        const dbUser = await User.findOne({ _id: id });
+        if (
+            !dbUser ||
+            dbUser.username !== req.body.username ||
+            decodedClient.username !== decodedSaved.username ||
+            decodedClient.id !== decodedSaved.id ||
+            dbUser.username !== decodedClient.username
+        ) {
+            throw new Error("Provided credentials do not match");
+        } else {
+            res.status(200);
+            res.json({ status: "success" });
+        }
     } catch (err) {
         console.error(err);
         res.status(500).json({
@@ -57,8 +74,9 @@ const loginAttempt = async (req, res) => {
 };
 
 const optionsPreflight = (req, res) => {
-    res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
     try {
+        res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
+        res.header("Strict-Transport-Security", "max-age=6000");
         res.header("Access-Control-Allow-Methods", "POST");
         res.header("Access-Control-Allow-Headers", [
             "content-type",

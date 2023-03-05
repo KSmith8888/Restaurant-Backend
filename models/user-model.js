3b4fc84d4a3fa268cdaed80fe20e12432d4952ca
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-03-04 17:55:54 -0800

    Add files via upload

---
+++ models/user-model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    admin: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const User = model("User", userSchema);
+
+export { User };

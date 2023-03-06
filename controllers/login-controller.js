08292e908a20b55008008e7281949058e5575778
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-03-05 18:52:01 -0800

    Pulled authorization into separate middleware

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -2,29 +2,22 @@ import jwt from "jsonwebtoken";
 
 import { User } from "../models/user-model.js";
 
-let adminToken;
-
 const createAccount = async (req, res) => {
     try {
         res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
         res.header("Strict-Transport-Security", "max-age=6000");
+        console.log(req.body);
         if (!req.body.username || !req.body.password) {
             throw new Error("Credentials not provided");
         } else {
             const userInfo = {
                 username: req.body.username,
                 password: req.body.password,
+                admin: req.body.admin ? true : false,
             };
             const dbUser = await User.create(userInfo);
-            const token = jwt.sign(
-                { id: dbUser._id, username: dbUser.username },
-                process.env.JWT_SECRET,
-                { expiresIn: "2h" }
-            );
-            adminToken = token;
-            res.status(200).json({ status: "success", token: token });
         }
-    } catch (error) {
+    } catch (err) {
         console.error(err);
         res.status(500).json({
             msg: "There has been an error, please try again later",
@@ -36,35 +29,28 @@ const loginAttempt = async (req, res) => {
     try {
         res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
         res.header("Strict-Transport-Security", "max-age=6000");
-        if (
-            !req.headers.authorization ||
-            !req.body.username ||
-            !req.body.password
-        ) {
-            throw new Error(
-                "Authentication failed, proper credentials are not present"
-            );
+        if (!req.body.username || !req.body.password) {
+            throw new Error("Credentials not provided");
         }
-        if (!req.headers.authorization.includes("Bearer ")) {
-            throw new Error("Invalid token format");
-        }
-        const token = req.headers.authorization.split(" ")[1];
-        const decodedClient = jwt.verify(token, process.env.JWT_SECRET);
-        const decodedSaved = jwt.verify(adminToken, process.env.JWT_SECRET);
-        const id = decodedClient.id;
-        const dbUser = await User.findOne({ _id: id });
+        const dbUser = await User.findOne({ username: req.body.username });
         if (
             !dbUser ||
             dbUser.username !== req.body.username ||
-            decodedClient.username !== decodedSaved.username ||
-            decodedClient.id !== decodedSaved.id ||
-            dbUser.username !== decodedClient.username
+            dbUser.password !== req.body.password
         ) {
             throw new Error("Provided credentials do not match");
-        } else {
-            res.status(200);
-            res.json({ status: "success" });
         }
+        const token = jwt.sign(
+            {
+                id: dbUser._id,
+                username: dbUser.username,
+                admin: dbUser.admin,
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: "2h" }
+        );
+        res.status(200);
+        res.json({ status: "success", token: token });
     } catch (err) {
         console.error(err);
         res.status(500).json({

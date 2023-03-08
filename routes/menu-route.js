7c9748e5113e48798d3a9fdaa12e083d77296999
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-03-07 17:39:22 -0800

    Added multer file handling

--- routes/menu-route.js
+++ routes/menu-route.js
@@ -1,4 +1,9 @@
 import express from "express";
+import multer from "multer";
+
+import { storage } from "../middleware/multer.js";
+
+const upload = multer({ storage: storage });
 
 import {
     getAllMenuItems,
@@ -9,14 +14,29 @@ import {
     optionsPreflight,
 } from "../controllers/menu-controller.js";
 
+import { sanitizeChars } from "../middleware/sanitize.js";
+import { authorizeUser } from "../middleware/authorize.js";
+
 const menuRouter = express.Router();
 
 menuRouter.options("/", optionsPreflight);
 menuRouter.options("/:id", optionsPreflight);
 menuRouter.get("/", getAllMenuItems);
-menuRouter.post("/", createMenuItem);
-menuRouter.delete("/:id", deleteMenuItem);
-menuRouter.get("/:id", getMenuItem);
-menuRouter.patch("/:id", updateMenuItem);
+menuRouter.post(
+    "/",
+    sanitizeChars,
+    authorizeUser,
+    upload.single("image"),
+    createMenuItem
+);
+menuRouter.delete("/:id", sanitizeChars, authorizeUser, deleteMenuItem);
+menuRouter.get("/:id", sanitizeChars, authorizeUser, getMenuItem);
+menuRouter.patch(
+    "/:id",
+    sanitizeChars,
+    authorizeUser,
+    upload.single("image"),
+    updateMenuItem
+);
 
 export { menuRouter };

341ec941e459656fc339fff49cc110d587d501f6
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-03-07 17:41:32 -0800

    Add files via upload

---
+++ middleware/multer.js
@@ -0,0 +1,20 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: (res, file, cb) => {
+        cb(null, "../static/uploads");
+    },
+    filename: (req, file, cb) => {
+        let fileExt;
+        if (file.originalname.includes(".jpg")) {
+            fileExt = ".jpg";
+        } else if (file.originalname.includes(".png")) {
+            fileExt = ".png";
+        }
+        const newName = `${Date.now()}-image${fileExt}`;
+        cb(null, newName);
+        req.filepath = `${process.env.BASE_URL}uploads/${newName}`;
+    },
+});
+
+export { storage };

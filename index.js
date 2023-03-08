45e01ab24bb42c847ed129c8cf085390c5c64917
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-03-07 17:40:56 -0800

    Added logout router
    Moved multer file handling down to menu router

--- index.js
+++ index.js
@@ -1,40 +1,23 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import multer from "multer";
 import mongoose from "mongoose";
 mongoose.set("strictQuery", false);
 
 import { menuRouter } from "./routes/menu-route.js";
 import { loginRouter } from "./routes/login-route.js";
+import { logoutRouter } from "./routes/logout-route.js";
 
 const app = express();
-
-const storage = multer.diskStorage({
-    destination: (res, file, cb) => {
-        cb(null, "./public/uploads");
-    },
-    filename: (req, file, cb) => {
-        let fileExt;
-        if (file.originalname.includes(".jpg")) {
-            fileExt = ".jpg";
-        } else if (file.originalname.includes(".png")) {
-            fileExt = ".png";
-        }
-        const newName = `${Date.now()}-image${fileExt}`;
-        cb(null, newName);
-        req.filepath = process.env.BASE_URL + newName;
-    },
-});
-const upload = multer({ storage: storage });
 
 app.use(express.static("./public"));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use("/api/v1/menu", upload.single("image"), menuRouter);
+app.use("/api/v1/menu", menuRouter);
 app.use("/api/v1/login", loginRouter);
+app.use("/api/v1/logout", logoutRouter);
 
 function startApp() {
     mongoose

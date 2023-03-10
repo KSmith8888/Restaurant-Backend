540e3bbdd149c65ec5c563b1ce260a02bdc804ab
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-03-09 18:18:40 -0800

    Updated validation

--- middleware/sanitize.js
+++ middleware/sanitize.js
@@ -2,6 +2,15 @@ function sanitizeChars(req, res, next) {
     try {
         const username = req.body.username;
         const password = req.body.password;
+        if (!username || !password) {
+            throw new Error("Username or password not provided");
+        }
+        if (
+            typeof req.body.username !== "string" ||
+            typeof req.body.password !== "string"
+        ) {
+            throw new Error("Username or password is not a string");
+        }
         const reg = new RegExp("^[a-zA-Z0-9 -.]+$");
         if (reg.test(username) && reg.test(password)) {
             next();

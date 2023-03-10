b051f1523de77f76e2ae720aabcd57706e541902
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-03-09 18:21:35 -0800

    Added authorize admin middleware

--- routes/login-route.js
+++ routes/login-route.js
@@ -6,11 +6,17 @@ import {
 } from "../controllers/login-controller.js";
 
 import { sanitizeChars } from "../middleware/sanitize.js";
-import { authorizeUser } from "../middleware/authorize.js";
+import { authorizeUser, authorizeAdmin } from "../middleware/authorize.js";
 
 const loginRouter = express.Router();
 
 loginRouter.post("/", sanitizeChars, loginAttempt);
-loginRouter.post("/create", sanitizeChars, authorizeUser, createAccount);
+loginRouter.post(
+    "/create",
+    sanitizeChars,
+    authorizeUser,
+    authorizeAdmin,
+    createAccount
+);
 
 export { loginRouter };

e57117f89ba8eb68773cc6021623cbde7fa5677d
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-03-09 18:14:14 -0800

    Added schedule router and updated static handling

--- index.js
+++ index.js
@@ -4,20 +4,24 @@ import express from "express";
 import mongoose from "mongoose";
 mongoose.set("strictQuery", false);
 
-import { menuRouter } from "./routes/menu-route.js";
 import { loginRouter } from "./routes/login-route.js";
+import { scheduleRouter } from "./routes/schedule-route.js";
+import { menuRouter } from "./routes/menu-route.js";
 import { logoutRouter } from "./routes/logout-route.js";
+import { authorizeUser, authorizeAdmin } from "./middleware/authorize.js";
 
 const app = express();
-
-app.use(express.static("./public"));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use("/api/v1/menu", menuRouter);
 app.use("/api/v1/login", loginRouter);
+app.use("/api/v1/schedule", scheduleRouter);
+app.use("/api/v1/menu", menuRouter);
 app.use("/api/v1/logout", logoutRouter);
+
+app.use(express.static("./public"));
+app.use(authorizeUser, authorizeAdmin, express.static("./static"));
 
 function startApp() {
     mongoose

8e69c20a38643cf96b866788699cdc955750a7b2
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-03-13 17:42:51 -0700

    Split out register user into separate route

--- routes/login-route.js
+++ routes/login-route.js
@@ -1,22 +1,10 @@
 import express from "express";
 
-import {
-    createAccount,
-    loginAttempt,
-} from "../controllers/login-controller.js";
-
+import { loginAttempt } from "../controllers/login-controller.js";
 import { sanitizeChars } from "../middleware/sanitize.js";
-import { authorizeUser, authorizeAdmin } from "../middleware/authorize.js";
 
 const loginRouter = express.Router();
 
 loginRouter.post("/", sanitizeChars, loginAttempt);
-loginRouter.post(
-    "/create",
-    sanitizeChars,
-    authorizeUser,
-    authorizeAdmin,
-    createAccount
-);
 
 export { loginRouter };

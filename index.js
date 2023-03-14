9eaa5228b77fe2e9869052ca4b5d2237b733ead2
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-03-13 17:37:12 -0700

    Split out app and added port fallback

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-import dotenv from "dotenv";
-dotenv.config();
-import express from "express";
 import mongoose from "mongoose";
 mongoose.set("strictQuery", false);
 
-import { loginRouter } from "./routes/login-route.js";
-import { scheduleRouter } from "./routes/schedule-route.js";
-import { menuRouter } from "./routes/menu-route.js";
-import { logoutRouter } from "./routes/logout-route.js";
-import { authorizeUser, authorizeAdmin } from "./middleware/authorize.js";
+import { app } from "./app.js";
 
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use("/api/v1/login", loginRouter);
-app.use("/api/v1/schedule", scheduleRouter);
-app.use("/api/v1/menu", menuRouter);
-app.use("/api/v1/logout", logoutRouter);
-
-app.use(express.static("./public"));
-app.use(authorizeUser, authorizeAdmin, express.static("./static"));
+const PORT = process.env.PORT || 3000;
 
 function startApp() {
     mongoose
         .connect(process.env.MONGO_DB_URI)
         .then(() => {
-            app.listen(process.env.PORT, () => {
-                console.log(`App is listening on port ${process.env.PORT}`);
+            app.listen(PORT, () => {
+                console.log(`App is listening on port ${PORT}`);
             });
         })
         .catch((err) => {

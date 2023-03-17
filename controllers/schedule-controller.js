b54a6a5bf12615a9c2435ef57ab3fd89deee6865
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-03-16 18:11:39 -0700

    Added create schedule functionality

--- controllers/schedule-controller.js
+++ controllers/schedule-controller.js
@@ -1,4 +1,5 @@
 import { Schedule } from "../models/schedule-model.js";
+import { User } from "../models/user-model.js";
 
 const getSchedule = async (req, res) => {
     try {
@@ -7,10 +8,15 @@ const getSchedule = async (req, res) => {
             "max-age=31536000; includeSubDomains"
         );
         const paramId = req.params.id;
-        const requestedItem = await Schedule.findOne({ _id: paramId });
-        if (requestedItem) {
+        if (!req.userId || paramId !== req.userId) {
+            throw new Error("User id not applied from token");
+        }
+        const requestedSchedule = await Schedule.findOne({
+            user_id: String(paramId),
+        });
+        if (requestedSchedule) {
             res.status(200);
-            res.json(requestedItem);
+            res.json(requestedSchedule);
         } else {
             res.status(404);
             res.json({ msg: "Item not found" });
@@ -25,14 +31,16 @@ const getSchedule = async (req, res) => {
 
 const getAllSchedules = async (req, res) => {
     try {
-        res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
         res.header(
             "Strict-Transport-Security",
             "max-age=31536000; includeSubDomains"
         );
-        const allMenuItems = await Schedule.find();
+        if (!req.admin) {
+            throw new Error("User not authorized for this route");
+        }
+        const allSchedules = await Schedule.find();
         res.status(200);
-        res.json(allMenuItems);
+        res.json(allSchedules);
     } catch (err) {
         console.error(err);
         res.status(500).json({
@@ -41,4 +49,38 @@ const getAllSchedules = async (req, res) => {
     }
 };
 
-export { getSchedule, getAllSchedules };
+const createSchedule = async (req, res) => {
+    try {
+        res.header(
+            "Strict-Transport-Security",
+            "max-age=31536000; includeSubDomains"
+        );
+        if (!req.admin) {
+            throw new Error("User not authorized for this route");
+        }
+        const scheduleName = req.body.name;
+        const scheduleData = req.body.scheduleInfo;
+        if (!scheduleName || !scheduleData) {
+            throw new Error("No username or schedule provided");
+        }
+        const dbUser = await User.findOne({ username: String(scheduleName) });
+        if (!dbUser) {
+            throw new Error("No database user found with provided username");
+        }
+        const newSchedule = {
+            user_id: dbUser._id,
+            name: dbUser.username,
+            schedule: scheduleData,
+        };
+        await Schedule.create(newSchedule);
+        res.status(200);
+        res.json({ msg: "Item created successfully" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            msg: "There has been an error, please try again later",
+        });
+    }
+};
+
+export { getSchedule, getAllSchedules, createSchedule };

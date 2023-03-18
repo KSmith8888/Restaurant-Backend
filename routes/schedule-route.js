cf2d1217007aa6c5d0eca9dc5eb4469d49f7436d
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-03-17 18:25:55 -0700

    Added patch and delete routes

--- routes/schedule-route.js
+++ routes/schedule-route.js
@@ -4,11 +4,17 @@ import { authorizeUser, authorizeAdmin } from "../middleware/authorize.js";
 import {
     getSchedule,
     getAllSchedules,
+    createSchedule,
+    updateSchedule,
+    deleteSchedule,
 } from "../controllers/schedule-controller.js";
 
 const scheduleRouter = express.Router();
 
 scheduleRouter.get("/", authorizeUser, authorizeAdmin, getAllSchedules);
 scheduleRouter.get("/:id", authorizeUser, getSchedule);
+scheduleRouter.post("/", authorizeUser, authorizeAdmin, createSchedule);
+scheduleRouter.patch("/:id", authorizeUser, authorizeAdmin, updateSchedule);
+scheduleRouter.delete("/:id", authorizeUser, authorizeAdmin, deleteSchedule);
 
 export { scheduleRouter };

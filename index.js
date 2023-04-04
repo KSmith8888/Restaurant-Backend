49a2c3b32ea22ac9441f95057e9c789adbaa4600
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-04-03 17:44:47 -0700

    Updated start app function to be async

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 import mongoose from "mongoose";
-mongoose.set("strictQuery", false);
 
 import { app } from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
-function startApp() {
-    mongoose
-        .connect(process.env.MONGO_DB_URI)
-        .then(() => {
-            app.listen(PORT, () => {
-                console.log(`App is listening on port ${PORT}`);
-            });
-        })
-        .catch((err) => {
-            console.error(err);
+async function startApp() {
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URI);
+        console.log("Connected to database");
+        app.listen(PORT, () => {
+            console.log(`App is listening on port ${PORT}`);
         });
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 startApp();
